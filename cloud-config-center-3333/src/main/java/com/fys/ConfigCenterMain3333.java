b0cb1664dd0e7c41527e1714c45eda3898962a0b
zhuyang <zy@starthope.com.cn>
2021-03-11 18:17:48 +0800

    1

---
+++ cloud-config-center-3333/src/main/java/com/fys/ConfigCenterMain3333.java
@@ -0,0 +1,13 @@
+package com.fys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3333 {
+     public static void main(String[] args) {
+           SpringApplication.run(ConfigCenterMain3333.class, args);
+      }
+}
